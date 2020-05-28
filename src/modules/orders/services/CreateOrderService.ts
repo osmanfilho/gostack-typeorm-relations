e34afba8bbbe4bf2ff02b21e16bb8817c48c864c
Osman Setuval <osman@ntsaobraz.com.br>
2020-05-28 17:57:53 -0300

    Commit Inicial

--- src/modules/orders/services/CreateOrderService.ts
+++ src/modules/orders/services/CreateOrderService.ts
@@ -18,16 +18,52 @@ interface IRequest {
 }
 
 @injectable()
-class CreateOrderService {
+class CreateProductService {
   constructor(
+    @inject('OrdersRepository')
     private ordersRepository: IOrdersRepository,
+
+    @inject('ProductsRepository')
     private productsRepository: IProductsRepository,
+
+    @inject('CustomersRepository')
     private customersRepository: ICustomersRepository,
   ) {}
 
   public async execute({ customer_id, products }: IRequest): Promise<Order> {
-    // TODO
+    const customer = await this.customersRepository.findById(customer_id);
+
+    if (!customer) {
+      throw new AppError('This customer does not exist');
+    }
+
+    const productsId = products.map(product => {
+      return { id: product.id };
+    });
+
+    const productsData = await this.productsRepository.findAllById(productsId);
+
+    const productsFinal = productsData.map(productData => {
+      const productFinal = products.find(
+        productFind => productFind.id === productData.id,
+      );
+
+      return {
+        product_id: productData.id,
+        price: productData.price,
+        quantity: productFinal?.quantity || 0,
+      };
+    });
+
+    const order = await this.ordersRepository.create({
+      customer,
+      products: productsFinal,
+    });
+
+    await this.productsRepository.updateQuantity(products);
+
+    return order;
   }
 }
 
-export default CreateOrderService;
+export default CreateProductService;
